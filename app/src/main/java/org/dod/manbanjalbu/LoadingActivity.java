7d05eb7f46d052cc80ac4b4f82408875277eaf2c
mincastle98 <alstjd98522@naver.com>
2021-08-20 16:53:17 +0900

    MODIFY file name
    - 로딩 페이지 파일명 변경

--- app/src/main/java/org/dod/manbanjalbu/LoadingActivity.java
+++ app/src/main/java/org/dod/manbanjalbu/LoadingActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class loading_page extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
