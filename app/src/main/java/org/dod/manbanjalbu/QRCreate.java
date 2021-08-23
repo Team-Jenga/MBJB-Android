0bf1a32155d263bb5ce8ca6afb5951c65dd3a9fc
minnseong <kdsvip5@naver.com>
2021-08-23 15:56:14 +0900

    ADD QRScanner & QRGenerator

---
+++ app/src/main/java/org/dod/manbanjalbu/QRCreate.java
@@ -0,0 +1,35 @@
+package org.dod.manbanjalbu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QRCreate extends AppCompatActivity {
+
+    private ImageView iv;
+    private String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcreate);
+
+        iv = (ImageView)findViewById(R.id.QRcode);
+        text = "https://park-duck.tistory.com";
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        } catch (Exception e){}
+    }
+}
